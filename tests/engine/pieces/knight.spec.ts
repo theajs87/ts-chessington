a0cbd9778389127d46f442a840aeac5b77f0e36e
Bianca <bianca.darolti@softwire.com>
2022-09-30 16:27:22 +0100

    Add tests that certain pieces cannot leave the board

--- tests/engine/pieces/knight.spec.ts
+++ tests/engine/pieces/knight.spec.ts
@@ -43,4 +43,15 @@ describe('Knight', () => {
 
         moves.should.deep.include(Square.at(2, 5));
     });
+
+    it('cannot leave the board', () => {
+        const knight = new Knight(Player.WHITE);
+        board.setPiece(Square.at(0, 0), knight);
+
+        const moves = knight.getAvailableMoves(board);
+
+        const expectedMoves = [Square.at(1, 2), Square.at(2, 1)];
+
+        moves.should.have.deep.members(expectedMoves);
+    });
 });
