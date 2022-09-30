320d35b0b594e4edfe68f581ab2961ae6ec89fad
Bianca <bianca.darolti@softwire.com>
2022-09-30 12:29:36 +0100

    Added a test for bishop movement

--- tests/engine/pieces/bishop.spec.ts
+++ tests/engine/pieces/bishop.spec.ts
@@ -1,5 +1,34 @@
 import Bishop from '../../../src/engine/pieces/bishop';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Bishop', () => {
+    let board: Board;
+    beforeEach(() => board = new Board());
 
+    it('can move diagonally', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(2, 3), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Forwards diagonal
+            Square.at(0, 1), Square.at(1, 2), Square.at(3, 4), Square.at(4, 5), Square.at(5, 6), Square.at(6, 7),
+            // Backwards diagonal
+            Square.at(0, 5), Square.at(1, 4), Square.at(3, 2), Square.at(4, 1), Square.at(5, 0)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(2, 3), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        moves.should.have.length(11);
+    });
 });
