e91826594fdd7eec8b0c900d247d1a0a150617ad
Bianca <bianca.darolti@softwire.com>
2022-09-30 15:09:56 +0100

    Added a test for king movement

--- tests/engine/pieces/king.spec.ts
+++ tests/engine/pieces/king.spec.ts
@@ -1,5 +1,32 @@
 import King from '../../../src/engine/pieces/king';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('King', () => {
+    let board: Board;
+    beforeEach(() => board = new Board());
 
+    it('can move to adjacent squares', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(3, 4), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        const expectedMoves = [
+            Square.at(2, 3), Square.at(2, 4), Square.at(2, 5), Square.at(3, 5),
+            Square.at(4, 5), Square.at(4, 4), Square.at(4, 3), Square.at(3, 3)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(3, 4), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.have.length(8);
+    });
 });
