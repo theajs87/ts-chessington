c10237afaa101c6eedc75c7cad3cdde719033917
Bianca <bianca.darolti@softwire.com>
2022-09-30 16:37:13 +0100

    Added a test that queens can take pieces

--- tests/engine/pieces/queen.spec.ts
+++ tests/engine/pieces/queen.spec.ts
@@ -3,6 +3,7 @@ import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
 import Pawn from '../../../src/engine/pieces/pawn';
+import King from '../../../src/engine/pieces/king';
 
 describe('Queen', () => {
     let board: Board;
@@ -69,5 +70,38 @@ describe('Queen', () => {
         const moves = queen.getAvailableMoves(board);
 
         moves.should.not.deep.include(Square.at(4, 7));
+    });
+
+    it('can take opposing pieces', () => {
+        const queen = new Queen(Player.WHITE);
+        const opposingPiece = new Pawn(Player.BLACK);
+        board.setPiece(Square.at(4, 4), queen);
+        board.setPiece(Square.at(4, 6), opposingPiece);
+
+        const moves = queen.getAvailableMoves(board);
+
+        moves.should.deep.include(Square.at(4, 6));
+    });
+
+    it('cannot take the opposing king', () => {
+        const queen = new Queen(Player.WHITE);
+        const opposingKing = new King(Player.BLACK);
+        board.setPiece(Square.at(4, 4), queen);
+        board.setPiece(Square.at(4, 6), opposingKing);
+
+        const moves = queen.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(4, 6));
+    });
+
+    it('cannot take friendly pieces', () => {
+        const queen = new Queen(Player.WHITE);
+        const friendlyPiece = new Pawn(Player.WHITE);
+        board.setPiece(Square.at(4, 4), queen);
+        board.setPiece(Square.at(4, 6), friendlyPiece);
+
+        const moves = queen.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(4, 6));
     });
 });
