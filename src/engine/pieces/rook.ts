df76a1cb3721468607189766ca696f43ae6b896c
Thea Sotthivej <Thea.Sotthivej@softwire.com>
2025-07-07 10:26:23 +0100

    Implement Rook

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,14 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let pos = board.findPiece(this);
+        let moves: Square[] = [];
+    
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            moves.push(new Square(pos.row, i));
+            moves.push(new Square(i, pos.col));
+        }
+
+        return moves.filter((square) => !((square.row === pos.row) && (square.col === pos.col)));
     }
 }
