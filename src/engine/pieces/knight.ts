46d0fbf28704ce9108ae88892550a6830d21de60
Thea Sotthivej <Thea.Sotthivej@softwire.com>
2025-07-07 12:13:26 +0100

    Implement Knight

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let pos = board.findPiece(this);
+        let moves = [];
+
+        for (let i = -1; i <= 1; i += 2) {
+            for (let j = -1; j <= 1; j += 2) {
+                moves.push(new Square(pos.row + 2 * i, pos.col + j));
+                moves.push(new Square(pos.row + i, pos.col + 2 * j));
+            }
+        }
+
+        return moves;
     }
 }
