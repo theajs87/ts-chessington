6ef5275b64dde7cf3697d2190fcbe869d7c1c726
LawanF <fathullahlawan@gmail.com>
2025-07-07 10:11:55 +0100

    implement double move on first pawn move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,17 +4,30 @@ import Board from '../board';
 import Square from '../square';
 
 export default class Pawn extends Piece {
+    startingRow = this.player === Player.WHITE ? 1 : 6
+
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
         let pos = board.findPiece(this);
+
         if (this.player === Player.WHITE) {
-            return new Square(pos.row + 1, pos.col);
+            let hasHadFirstMove: Boolean = !(pos.row == this.startingRow);
+            let moves: Square[] = new Array(new Square(pos.row + 1, pos.col));
+            if (!hasHadFirstMove) {
+                moves.push(new Square(pos.row + 2, pos.col));
+            }
+            return moves;
         }
         else {
-            return new Square(pos.row - 1, pos.col);
+            let hasHadFirstMove: Boolean = !(pos.row == this.startingRow);
+            let moves: Square[] = new Array(new Square(pos.row - 1, pos.col));
+            if (!hasHadFirstMove) {
+                moves.push(new Square(pos.row - 2, pos.col));
+            }
+            return moves;
         }
     }
 }
